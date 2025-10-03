cf5efbc099872dc84c9994c6246ca551cffe85f4
Radames <rjhalmeman@gmail.com>
2025-10-03 05:13:28 -0300

    03/10/2025 - 05:13:28

--- backend/server.js
+++ backend/server.js
@@ -78,28 +78,6 @@ const menuRoutes = require('./routes/menuRoutes');
 app.use('/menu', menuRoutes);
 
 
-
-// const pessoaRoutes = require('./routes/pessoaRoutes');
-// app.use('/pessoa', pessoaRoutes);
-
-
-
-// const professorRoutes = require('./routes/professorRoutes');
-// app.use('/professor', professorRoutes);
-
-// const avaliadorRoutes = require('./routes/avaliadorRoutes');
-// app.use('/avaliador', avaliadorRoutes);
-
-// const avaliadoRoutes = require('./routes/avaliadoRoutes');
-// app.use('/avaliado', avaliadoRoutes);
-
-
-// const avaliacaoRoutes = require('./routes/avaliacaoRoutes');
-// app.use('/avaliacao', avaliacaoRoutes);
-
-// const avaliacaoHasQuestaoRoutes = require('./routes/avaliacaoHasQuestaoRoutes');
-// app.use('/avaliacaoHasQuestao', avaliacaoHasQuestaoRoutes);
-
 const cargoRoutes = require('./routes/cargoRoutes');
 app.use('/cargo', cargoRoutes);
 
